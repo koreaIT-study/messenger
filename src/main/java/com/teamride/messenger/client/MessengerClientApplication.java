a45e4e5dd14dc0a2ebb9a22acc4e4022f9d5ace2
shmin7777 <shmin7777@naver.com>
2022-10-06 00:08:41 +0900

    init

---
+++ src/main/java/com/teamride/messenger/client/MessengerClientApplication.java
@@ -0,0 +1,13 @@
+package com.teamride.messenger.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamride/messenger/client/MessengerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamride.messenger.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

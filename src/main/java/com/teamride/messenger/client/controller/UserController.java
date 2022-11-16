1b49ca11e9e22f01607a306f0f6a603a7cdbf3d3
dongjin <dongjin.irolab@gmail.com>
2022-11-16 14:43:19 +0900

    기존 html 수정 및 기본경로 설정, ajax js파일 추가(후에 삭제가능)

--- src/main/java/com/teamride/messenger/client/controller/UserController.java
+++ src/main/java/com/teamride/messenger/client/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,6 +31,14 @@ public class UserController {
         return mv;
     }
 
+    @GetMapping("/sign_in")
+    public ModelAndView singIn() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("sign_in");
+
+        return mv;
+    }
+
     @GetMapping("/sign_up")
     public ModelAndView signUp() {
         ModelAndView mv = new ModelAndView();
@@ -49,6 +58,7 @@ public class UserController {
                 .build();
     }
 
+    @ResponseBody
     @PostMapping("/loginAction")
     public RestResponse loginAction(@RequestBody AdminDTO adminDTO) {
         Mono<RestResponse> resp = webClient.mutate().baseUrl("http://localhost:12000")
@@ -64,6 +74,7 @@ public class UserController {
         return resp.block();
     }
 
+    @ResponseBody
     @GetMapping("/smtpRequest")
     public RestResponse smtpRequest(@RequestParam String email) {
         log.debug("email {}", email);
@@ -78,6 +89,7 @@ public class UserController {
         return resp.block();
     }
 
+    @ResponseBody
     @PostMapping("/signUp")
     public RestResponse signUp(@RequestBody AdminDTO adminDTO) {
         Mono<RestResponse> resp = webClient.mutate().baseUrl("http://localhost:12000")
